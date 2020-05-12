395ccf0a85060b2519750a51607a0bd42acfd8d7
Viktoras <vikter@inbox.lt>
2020-05-12 07:45:53 +0300

    Remove all todos

--- js/todo.js
+++ js/todo.js
@@ -25,9 +25,17 @@ function renderList(list) {
     
     return listPlace.innerHTML += HTML;
 }
+
+
+/********************************
+    GENERATE CONTENT
+********************************/
+
 renderList(todo_list);
 
-
+/********************************
+    REMOVE SINGLE TODO ITEM
+********************************/
 const removeActions = document.querySelectorAll('.item .action.remove');
 
 
@@ -39,4 +47,23 @@ for(let i=0; i<removeActions.length; i++){
 function actionRemoveTodoItem( event ){
     const parentItem= event.target.closest('.item');
     parentItem.remove();
+}
+
+/********************************
+    REMOVE ALL TODO ITEMS
+********************************/
+
+const BTNremoveAll =document.querySelector('.global-actions > .action.remove');
+
+BTNremoveAll.addEventListener('click', actionRemoveAllTodoItems);
+
+function actionRemoveAllTodoItems ( event ) {
+    const parent = event.target.closest('.container');
+    const allTodoItems = parent.querySelectorAll('.item');
+    console.log(allTodoItems);
+
+    for( let i=0; i<allTodoItems.length; i++ ){
+        allTodoItems[i].remove();
+    }
+    
 }
